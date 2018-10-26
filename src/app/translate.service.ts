a0cc14693c7e8d234b4d471f4dc20c38de3424de
Dominik <git@dafnik.me>
2018-10-26 17:01:57 +0200

    Change default language to german

--- src/app/translate.service.ts
+++ src/app/translate.service.ts
@@ -12,9 +12,9 @@ export class TranslateService {
 
     if (lang === 'DEFAULT') {
       if (this.cookieService.get('language') == null) {
-        this.cookieService.put('language', 'en', { expires: 'Tue, 24-Jan-2050 12:12:12 GMT'});
-        lang = 'en';
-        console.log('No language cookie found! Using english as default');
+        this.cookieService.put('language', 'de', { expires: 'Tue, 24-Jan-2050 12:12:12 GMT'});
+        lang = 'de';
+        console.log('No language cookie found! Using de as default');
       } else {
         lang = this.cookieService.get('language');
         console.log('Language cookie found! Using ' + lang);
@@ -26,7 +26,7 @@ export class TranslateService {
     this.cookieService.put('language', lang, { expires: 'Tue, 24-Jan-2050 12:12:12 GMT'});
 
     return new Promise<{}>((resolve, reject) => {
-      const langPath = `assets/i18n/${lang || 'en'}.json`;
+      const langPath = `assets/i18n/${lang || 'de'}.json`;
 
       this.http.get<{}>(langPath).subscribe(
         translation => {
