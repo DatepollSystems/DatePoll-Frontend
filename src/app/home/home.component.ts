c61f69901a5afad8cafc1f4df8a32ce84c8d7514
Dominik <git@dafnik.me>
2018-11-13 09:15:31 +0100

    Add home component with side nav

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {MyUserService} from '../auth/my-user.service';
 import {Subscription} from 'rxjs';
 
@@ -8,18 +8,38 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  navBarOpened = false;
+  navBarMode = 'over';
+
   firstname: string;
   surname: string;
   email: string;
 
   showCinema = true;
-  showPoll = true;
+  showPoll = false;
 
   private firstnameSubscription: Subscription;
   private surnameSubscription: Subscription;
   private emailSubscription: Subscription;
 
-  constructor(private myUserService: MyUserService) {
+  constructor(private ngZone: NgZone, private myUserService: MyUserService) {
+    if ((window.screen.width) > 992) {
+      this.navBarOpened = true;
+      this.navBarMode = 'side';
+    }
+
+    window.onresize = () => {
+      this.ngZone.run(() => {
+        if ((window.screen.width) > 992) {
+          this.navBarOpened = true;
+          this.navBarMode = 'side';
+        } else {
+          this.navBarOpened = false;
+          this.navBarMode = 'over';
+        }
+      });
+    };
+
     this.firstname = this.myUserService.getFirstname();
     this.surname = this.myUserService.getSurname();
     this.email = this.myUserService.getEmail();
