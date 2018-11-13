2d12f797457fd85f02ccf7910d0a25c468962e97
Dominik <git@dafnik.me>
2018-11-13 09:14:50 +0100

    Add footer

--- src/app/footer/modals/feedback-modal/feedback-modal.component.ts
+++ src/app/footer/modals/feedback-modal/feedback-modal.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import {MzBaseModal} from 'ngx-materialize';
 
 @Component({
   selector: 'app-feedback-modal',
   templateUrl: './feedback-modal.component.html',
   styleUrls: ['./feedback-modal.component.css']
 })
-export class FeedbackModalComponent extends MzBaseModal {
+export class FeedbackModalComponent {
   sendBrowserData = false;
+  feedback: string;
 
   sendFeedback() {
+    console.log('Feedback:' + this.feedback);
+
     console.log('Send browser data:' + this.sendBrowserData);
 
     if (this.sendBrowserData) {
