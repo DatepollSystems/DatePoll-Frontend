6db00fe3358777cd5318a18e039f4fbf6af70e52
Dominik <git@dafnik.me>
2019-01-28 18:18:07 +0100

    Fix url bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ declare const require: any;
 export const environment = {
   production: true,
   version: require('../../package.json').version,
-  apiUrl: 'https://datepoll-backend.dafnik.me/api/'
+  apiUrl: 'https://datepoll-backend.dafnik.me/api'
 };
