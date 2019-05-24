7f3e6a7676bdb6536aa43b8a21722173657d6372
Dominik <git@dafnik.me>
2019-05-24 11:47:13 +0200

    Order imports

--- src/app/home/management/users-management/users-management.module.ts
+++ src/app/home/management/users-management/users-management.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {CustomFormsModule} from 'ng2-validation';
+import {NgxMatSelectSearchModule} from 'ngx-mat-select-search';
 
 import {MaterialModule} from '../../../material-module';
 import {TranslationModule} from '../../../translation/translation.module';
@@ -11,9 +12,8 @@ import {UsersManagementRoutingModule} from './users-management-routing.module';
 import {UsersExportBottomSheetComponent, UsersManagementComponent} from './users-management.component';
 import {UserCreateModalComponent} from './user-create-modal/user-create-modal.component';
 import {UserUpdateModalComponent} from './user-update-modal/user-update-modal.component';
-import { PerformanceBadgeSelectComponent } from './performance-badge-select/performance-badge-select.component';
-import { InstrumentSelectComponent } from './instrument-select/instrument-select.component';
-import {NgxMatSelectSearchModule} from 'ngx-mat-select-search';
+import {PerformanceBadgeSelectComponent} from './performance-badge-select/performance-badge-select.component';
+import {InstrumentSelectComponent} from './instrument-select/instrument-select.component';
 
 @NgModule({
   declarations: [
@@ -36,8 +36,9 @@ import {NgxMatSelectSearchModule} from 'ngx-mat-select-search';
     CustomFormsModule,
     MaterialModule,
     TranslationModule,
-    UsersManagementRoutingModule,
-    NgxMatSelectSearchModule
+    NgxMatSelectSearchModule,
+    UsersManagementRoutingModule
   ]
 })
-export class UsersManagementModule {}
+export class UsersManagementModule {
+}
