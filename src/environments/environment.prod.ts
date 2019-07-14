3a6b67d18803ef4ab1cfb8ba7dc65151e4e477d7
Dominik <git@dafnik.me>
2019-07-14 20:35:02 +0200

    Add keys.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,8 @@ export const environment = {
   production: true,
   version: require('../../package.json').version,
   required_version: require('../../package.json').required_version,
-  apiUrl: 'https://' + window.location.hostname + ':8443/api'
+  apiUrl: 'https://' + window.location.hostname + ':8443/api',
+
+  cinema_weatherforecast_openweathermap_city_id: require('../../keys.json').cinema_weatherforecast_openweathermap_city_id,
+  cinema_weatherforecast_openweathermap_api_key: require('../../keys.json').cinema_weatherforecast_openweathermap_api_key,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@ export const environment = {
   production: false,
   version: require('../../package.json').version,
   required_version: require('../../package.json').required_version,
-  apiUrl: 'http://' + window.location.hostname + ':8000/api'
+  apiUrl: 'http://' + window.location.hostname + ':8000/api',
+
+  cinema_weatherforecast_openweathermap_city_id: require('../../keys.json').cinema_weatherforecast_openweathermap_city_id,
+  cinema_weatherforecast_openweathermap_api_key: require('../../keys.json').cinema_weatherforecast_openweathermap_api_key,
 };
