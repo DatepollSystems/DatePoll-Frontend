0cf806c62141bdb752292907efb61a7ebeef1ea9
Dominik <git@dafnik.me>
2019-11-12 19:39:11 +0100

    Change production port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: true,
   version: require('../../package.json').version,
   required_version: require('../../package.json').required_version,
-  apiUrl: 'https://' + window.location.hostname + ':9130/api'
+  apiUrl: 'https://' + window.location.hostname + ':9230/api'
 };
