d5a211dbc5109425594d82d31a98d9dc155863c6
Dominik <git@dafnik.me>
2019-12-27 13:56:17 +0100

    [feature] Adds maps component

---
+++ src/app/services/shared-components/maps-component/maps-component.component.ts
@@ -0,0 +1,105 @@
+import {AfterViewInit, Component, EventEmitter, Input, Output} from '@angular/core';
+import * as L from 'leaflet';
+import {icon, Marker} from 'leaflet';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-maps-component',
+  templateUrl: './maps-component.component.html',
+  styleUrls: ['./maps-component.component.css']
+})
+export class MapsComponentComponent implements AfterViewInit {
+  private map;
+  private marker;
+  @Input()
+  public x: number;
+  @Input()
+  public y: number;
+
+  private searchFilter: string;
+
+  @Input()
+  public showMarker = false;
+  @Input()
+  public edible = false;
+
+  @Output()
+  public coordinatesChanged = new EventEmitter();
+
+  private randomMapId = 0;
+
+  constructor(private http: HttpClient) {
+    this.randomMapId = Math.random();
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap(this.x, this.y);
+    if (this.showMarker) {
+      this.drawMarker();
+    }
+  }
+
+  search() {
+    this.http.get('https://geocode.xyz/' + this.searchFilter + '?json=1').subscribe(
+      (response: any) => {
+        console.log(response);
+        this.map.panTo(new L.LatLng(response.latt, response.longt));
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  private initMap(x: number, y: number): void {
+    this.map = L.map(this.randomMapId.toString(), {
+      center: [x, y],
+      zoom: 12
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+    if (this.edible) {
+      this.map.on('click', (event: any) => {
+        this.x = event.latlng.lat;
+        this.y = event.latlng.lng;
+        this.drawMarker();
+
+        const coordinates = {
+          'x': this.x,
+          'y': this.y
+        };
+        this.coordinatesChanged.emit(coordinates);
+
+        console.log(event.latlng.lat + ' ' + event.latlng.lng);
+      });
+    }
+
+  }
+
+  drawMarker() {
+    if (this.marker != null) {
+      this.marker.removeFrom(this.map);
+    }
+
+    const iconRetinaUrl = 'assets/marker-icon-2x.png';
+    const iconUrl = 'assets/marker-icon.png';
+    const shadowUrl = 'assets/marker-shadow.png';
+    Marker.prototype.options.icon = icon({
+      iconRetinaUrl,
+      iconUrl,
+      shadowUrl,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41]
+    });
+
+    this.marker = L.marker([this.x, this.y]).addTo(this.map);
+  }
+
+}
