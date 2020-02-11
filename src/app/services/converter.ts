7dad0f34e54a324000df5311d43e0948c63a5509
Dominik <git@dafnik.me>
2020-02-11 19:21:13 +0100

    [fix] Finally fix iOS Safari date bug
    The bug is fixed via getIOSDate method in the converter. It only occurs if there are hours and minutes involved. For example: "2020-10-05 22:25" has to replaced to "2020-10-05T22:25".

--- src/app/services/converter.ts
+++ src/app/services/converter.ts
@@ -48,21 +48,7 @@ export class Converter {
   }
 
   public static getIOSDate(value: string): Date {
-    const date = new Date(value);
-
-    let month = '' + (date.getMonth() + 1);
-    let day = '' + date.getDate();
-
-    if (month.length < 2) {
-      month = '0' + month;
-    }
-    if (day.length < 2) {
-      day = '0' + day;
-    }
-
-    const toReturnDate = new Date(
-      date.getFullYear() + '-' + month + '-' + day + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
-    );
-    return toReturnDate;
+    const temp = value.toString().replace(' ', 'T');
+    return new Date(temp);
   }
 }
