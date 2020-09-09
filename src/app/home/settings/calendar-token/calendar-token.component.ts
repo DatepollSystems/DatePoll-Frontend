1a3687fe3035413ef2b090f4d758220315e98499
Dominik <git@dafnik.me>
2020-09-09 19:33:29 +0200

    [ref] Remove .ics from calendar routes

--- src/app/home/settings/calendar-token/calendar-token.component.ts
+++ src/app/home/settings/calendar-token/calendar-token.component.ts
@@ -36,7 +36,7 @@ export class CalendarTokenComponent {
     selBox.style.left = '0';
     selBox.style.top = '0';
     selBox.style.opacity = '0';
-    selBox.value = val + '.ics';
+    selBox.value = val;
     document.body.appendChild(selBox);
     selBox.focus();
     selBox.select();
