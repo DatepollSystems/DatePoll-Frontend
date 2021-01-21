3f9ebe17a398a157f9986804abb6cff1d9f32078
Dominik <git@dafnik.me>
2021-01-21 14:51:59 +0100

    [fix] Add english language selector and fix language file bugs

--- languages.sh
+++ languages.sh
@@ -6,6 +6,8 @@ main () {
  node ./translation/generateTranslation.js ./src/assets/i18n/de.json it
  node ./translation/generateTranslation.js ./src/assets/i18n/de.json pt
 
+ prettier --config ./.prettierrc --write ./src/assets/i18n
+
  printf "\nFinished!."
 }
 time main
