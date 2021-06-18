31066570a77f97f539715ab434aabd21cc97758c
Dominik <git@dafnik.me>
2021-06-18 10:39:17 +0200

    [feat] Add event series creation

--- src/app/home/events/events-administration/event-create/event-create.component.ts
+++ src/app/home/events/events-administration/event-create/event-create.component.ts
@@ -1,10 +1,8 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, ViewEncapsulation} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Router} from '@angular/router';
-import {MatSnackBar} from '@angular/material/snack-bar';
 import {Subscription} from 'rxjs';
 
-import {TranslateService} from '../../../../translation/translate.service';
 import {GroupsService} from '../../../management/groups-management/groups.service';
 import {EventsService} from '../../services/events.service';
 import {StandardDecisionsService} from '../../services/standardDecisions.service';
@@ -18,11 +16,14 @@ import {
 import {EventDecision} from '../../models/event-decision.model';
 import {EventDate} from '../../models/event-date.model';
 import {Event} from '../../models/event.model';
+import {MatCalendarCellClassFunction} from '@angular/material/datepicker';
+import {NotificationService} from '../../../../utils/notification.service';
 
 @Component({
   selector: 'app-event-create',
   templateUrl: './event-create.component.html',
   styleUrls: ['./event-create.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EventCreateComponent implements OnDestroy {
   allMembers = false;
@@ -35,12 +36,15 @@ export class EventCreateComponent implements OnDestroy {
 
   dates: EventDate[] = [];
 
+  xWeeks = 1;
+  xRepeats: number;
+  repeats: any[] = [];
+
   constructor(
     private groupsService: GroupsService,
     private router: Router,
-    private snackBar: MatSnackBar,
+    private notificationService: NotificationService,
     private standardDecisionsService: StandardDecisionsService,
-    private translate: TranslateService,
     private eventsService: EventsService,
     private eventsCreateUpdateService: EventsCreateUpdateService
   ) {
@@ -93,15 +97,61 @@ export class EventCreateComponent implements OnDestroy {
     this.allMembers = checked;
   }
 
+  addRepeating(form: NgForm) {
+    if (this.dates.length === 0) {
+      this.notificationService.info('EVENTS_ADMINISTRATION_CREATE_EVENT_FORM_DATE_LIST_REQUIRED');
+      return;
+    }
+    console.log(this.dates);
+    console.log('xWeeks:' + this.xWeeks);
+    console.log('xRepeats:' + this.xRepeats);
+    for (let i = 1; i <= this.xRepeats; i++) {
+      const currentDates = [];
+      for (const cDate of this.dates.slice()) {
+        const date = new Date(cDate.date);
+        date.setDate(date.getDate() + i * this.xWeeks * 7);
+        currentDates.push(new EventDate(cDate.id, cDate.location, cDate.x, cDate.y, date, cDate.description));
+      }
+      this.repeats.push({
+        xRepeats: i,
+        dates: currentDates,
+      });
+    }
+    form.reset();
+  }
+
+  deleteRepeats(repeat) {
+    const i = this.repeats.indexOf(repeat);
+    if (i > -1) {
+      this.repeats.splice(i, 1);
+    }
+  }
+
+  isSelected: MatCalendarCellClassFunction<Date> = (event, view) => {
+    for (const repeat of this.repeats) {
+      for (const date of repeat.dates) {
+        if (
+          date.date.getFullYear() === event.getFullYear() &&
+          date.date.getMonth() === event.getMonth() &&
+          date.date.getDate() === event.getDate()
+        ) {
+          return 'selected';
+        }
+      }
+    }
+
+    return '';
+  };
+
   create(form: NgForm) {
     if (this.dates.length === 0) {
-      this.snackBar.open(this.translate.getTranslationFor('EVENTS_ADMINISTRATION_CREATE_EVENT_FORM_DATE_LIST_REQUIRED'));
+      this.notificationService.info('EVENTS_ADMINISTRATION_CREATE_EVENT_FORM_DATE_LIST_REQUIRED');
       return;
     }
 
     if (this.joined.length === 0 && !this.allMembers) {
       console.log('Groups length 0! - ' + this.joined.length + ' And allMembers - ' + this.allMembers);
-      this.snackBar.open(this.translate.getTranslationFor('BROADCASTS_ADMINISTRATION_CREATE_NOTIFICATION_NO_GROUPS_AND_NOT_ALL_MEMBERS'));
+      this.notificationService.info('BROADCASTS_ADMINISTRATION_CREATE_NOTIFICATION_NO_GROUPS_AND_NOT_ALL_MEMBERS');
       return;
     }
 
@@ -111,39 +161,48 @@ export class EventCreateComponent implements OnDestroy {
 
     this.router.navigateByUrl('/home/events/administration');
 
-    const event = new Event(0, name, currentDate, currentDate, this.allMembers, description, this.decisions, this.dates);
-    console.log(event);
-    this.eventsCreateUpdateService.createEvent(event).subscribe(
-      (response: any) => {
-        console.log(response);
-        this.eventsService.fetchEvents();
-        this.snackBar.open(this.translate.getTranslationFor('EVENTS_ADMINISTRATION_CREATE_EVENT_SUCCESSFULLY_CREATED'));
+    const eventsToCreate = [];
+    eventsToCreate.push(new Event(0, name, currentDate, currentDate, this.allMembers, description, this.decisions, this.dates));
 
-        const id = response.event.id;
+    for (const repeat of this.repeats) {
+      eventsToCreate.push(new Event(0, name, currentDate, currentDate, this.allMembers, description, this.decisions, repeat.dates));
+    }
 
-        if (this.joined.length > 0) {
-          console.log('create Event | Adding groups and subgroups to event');
+    console.log(eventsToCreate);
 
-          for (const group of this.joined) {
-            if (group.type === GroupType.PARENTGROUP) {
-              this.eventsCreateUpdateService.addGroupToEvent(id, group.id).subscribe(
-                (sdata: any) => {
-                  console.log(sdata);
-                },
-                (error) => console.log(error)
-              );
-            } else {
-              this.eventsCreateUpdateService.addSubgroupToEvent(id, group.id).subscribe(
-                (sdata: any) => {
-                  console.log(sdata);
-                },
-                (error) => console.log(error)
-              );
+    for (const event of eventsToCreate) {
+      this.eventsCreateUpdateService.createEvent(event).subscribe(
+        (response: any) => {
+          console.log(response);
+          this.eventsService.fetchEvents();
+          this.notificationService.info('EVENTS_ADMINISTRATION_CREATE_EVENT_SUCCESSFULLY_CREATED');
+
+          const id = response.event.id;
+
+          if (this.joined.length > 0) {
+            console.log('create Event | Adding groups and subgroups to event');
+
+            for (const group of this.joined) {
+              if (group.type === GroupType.PARENTGROUP) {
+                this.eventsCreateUpdateService.addGroupToEvent(id, group.id).subscribe(
+                  (sdata: any) => {
+                    console.log(sdata);
+                  },
+                  (error) => console.log(error)
+                );
+              } else {
+                this.eventsCreateUpdateService.addSubgroupToEvent(id, group.id).subscribe(
+                  (sdata: any) => {
+                    console.log(sdata);
+                  },
+                  (error) => console.log(error)
+                );
+              }
             }
           }
-        }
-      },
-      (error) => console.log(error)
-    );
+        },
+        (error) => console.log(error)
+      );
+    }
   }
 }
