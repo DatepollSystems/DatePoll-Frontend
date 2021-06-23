901f90a03ff24fad08f01e082be78eca5f749d1a
Dominik <git@dafnik.me>
2021-06-23 16:25:53 +0200

    [fix] Fix update bug

--- src/app/broadcast-download/broadcast-download.component.ts
+++ src/app/broadcast-download/broadcast-download.component.ts
@@ -25,7 +25,6 @@ export class BroadcastDownloadComponent implements OnInit {
         this.token = token;
         console.log('File to download: ' + token);
         const link = document.createElement('a');
-        link.download = name;
         link.href = this.apiUrl + token;
         document.body.appendChild(link);
         // If there is no file for this token the redirects happens on the server to not-found
